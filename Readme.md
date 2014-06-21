4776521532a9f50aee99fd885b7608ea34830271
JanEggers <jan.Eggers@ferag.com>
2014-06-21 14:31:35 +0200

    generate css from less

--- Readme.md
+++ Readme.md
@@ -7,3 +7,10 @@ http://www.jetbrains.com/webstorm/webhelp/running-and-debugging-node-js.html
 
 
 npm install from cmd line will install missing packages specified in package.json ->.dependencies
+
+
+bower install from cmd line will install missing packages specified in bower.json ->.dependencies
+
+use 
+npm install gulp -g //installs gulp globally
+gulp                // use gulp to compile / stylcheck / convert stuff
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,39 @@
 'use strict';
 
 var gulp = require('gulp');
+var less = require('gulp-less');
 var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
 
 var paths = {
-    scripts: ['FrontEnd/Model/**/*.js']
+    in: {
+        scripts: ['./FrontEnd/Model/**/*.js'],
+        less: ['./FrontEnd/less/**/*.less']
+    },
+    out: {
+        css: './FrontEnd/css/'
+    }
 };
 
+// convert less to css and concat it to one css file
+gulp.task('less', function () {
+    gulp.src(paths.in.less)
+        .pipe(less())
+        .pipe(concat('default.css'))
+        .pipe(gulp.dest(paths.out.css));
+});
+
+//detect js errors
 gulp.task('lint', function () {
-    return gulp.src(paths.scripts)
+    return gulp.src(paths.in.scripts)
         .pipe(jshint())
         .pipe(jshint.reporter());
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function () {
-    gulp.watch(paths.scripts, ['lint']);
-    gulp.watch(paths.images, ['images']);
+    gulp.watch(paths.in.scripts, ['lint']);
+    gulp.watch(paths.in.less, ['less']);
 });
 
-gulp.task('default', ['watch', 'lint']);
+gulp.task('default', ['watch', 'lint', 'less']);
