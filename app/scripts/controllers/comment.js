5351c3de7a0004306e8ec606b06dff9bd4e0c3f7
Franca Rast <franca.rast@screenconcept.ch>
2014-09-24 18:34:35 +0200

    show comments

--- app/scripts/controllers/comment.js
+++ app/scripts/controllers/comment.js
@@ -8,14 +8,9 @@
  * Controller of the redditcloneApp
  */
 angular.module('redditcloneApp')
-  .controller('CommentCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.comments = [
-      { id: 1, name: "Hoegger", prename: "Andreas", email: "andreas.hoegger@bsiag.com" },
-      { id: 2, name: "Dietrich", prename: "Benno", email: "benno.dietrich@soxes.ch" },
-    ];
-  });
+  .controller('CommentCtrl', ['$http', '$scope', function ($http, $scope) {
+    $scope.comments = [ ];
+    $http.get('/data/comments').success(function (data) {
+        $scope.comments = data;
+    });
+  } ]);
