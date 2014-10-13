0dec4ab93d2f19db3538b7ebcd5e447df2ba1f86
d1heller <d_heller@gmx.ch>
2014-10-13 20:03:50 +0200

    deleted unused function of 'PostsCtrl'

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -63,12 +63,6 @@
         localStorageService.remove('postForm');
         $window.history.back();
       };
-
-      $scope.score = 0;
-      $scope.calcScore = function (rating) {
-        var s = $scope.score;
-        $scope.score = s + rating;
-      };
     }])
     .controller('PostCtrl', ['$scope', '$injector', function ($scope, $injector) {
       var userRepository = $injector.get('userRepository');
