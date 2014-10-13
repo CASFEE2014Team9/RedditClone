103931ae81a713c4150b13edc6dbb4ab8b58563d
d1heller <d_heller@gmx.ch>
2014-10-13 15:26:39 +0200

    new post 'create' and 'cancel' functionality  - check login state  - use local storage

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -13,16 +13,54 @@
    * Controller of the redditcloneApp
    */
   angular.module('redditcloneApp')
-    .controller('PostsCtrl', ['$window', '$scope', '$q', 'postRepository', function ($window, $scope, $q, postRepository) {
+    .controller('PostsCtrl', ['$window', '$scope', '$q', 'postRepository', 'localStorageService', 'session', '$location', function ($window, $scope, $q, postRepository, localStorageService, session, $location) {
       $scope.posts = postRepository.getAll().then(function (data) {
         $scope.posts = data;
       });
 
-      $scope.create = function () {
-        // save link and description...
+      // load 'post' form information from local storage
+      $scope.post = localStorageService.get('postForm');
+
+      // callback function -> if 'post' form data changed
+      var onDataChanged = function (newValue, oldValue) {
+        if ($scope.post === undefined) {
+          return;
+        }
+
+        // save 'post' form information in local storage
+        localStorageService.set('postForm', $scope.post);
       };
 
+      // 'watch' (->call callback if data changed) post form changes
+      $scope.$watch('post.url', onDataChanged);
+      $scope.$watch('post.description', onDataChanged);
+
+      // save 'url' and 'description' of post form page
+      // called from 'newpost.html'...
+      $scope.create = function () {
+        // check user already logged in...
+        if (!session.isLoggedIn()) {
+          // ...if not, open login page
+          $location.path('/login');
+          return;
+        }
+
+        // ... otherwise get user id
+        $scope.post.userId = session.user.data.id;
+
+        // update post (data)
+        postRepository.post($scope.post);
+
+        // clear 'post' form and local storage
+        $scope.post = {};
+        localStorageService.remove('postForm');
+      };
+
+      // cancel creating new post, go back to last page
+      // called from 'newpost.html'...
       $scope.cancel = function () {
+        // clear local storage
+        localStorageService.remove('postForm');
         $window.history.back();
       };
     }]);
